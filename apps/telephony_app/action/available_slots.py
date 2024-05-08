1b48675b958ef7b2471a128f2733ce0bce562935
sauravkumal <sk.sauravkumal1@gmail.com>
2024-05-08 20:24:02 +0545

    attribute error: AVAILABLE_SLOTS

---
+++ apps/telephony_app/action/available_slots.py
@@ -0,0 +1,59 @@
+from typing import Optional, Type
+from pydantic.v1 import BaseModel, Field
+import requests
+import json
+
+from vocode.streaming.action.base_action import BaseAction
+from vocode.streaming.models.actions import (
+    ActionConfig,
+    ActionInput,
+    ActionOutput,
+    ActionType,
+)
+
+
+class AvailableSlotsActionConfig(ActionConfig, type=ActionType.AVAILABLE_SLOTS):
+    base_url: str
+
+
+class AvailableSlotsParameters(BaseModel):
+    postal_code: str = Field(..., description="The postal code of the address where the repair job is to be booked")
+    appliance_type: str = Field(..., description="The appliance type can either be gas or electric. If not provided, "
+                                                 "its electric by default")
+
+
+class AvailableSlotsResponse(BaseModel):
+    success: str
+
+
+class AvailableSlots(
+    BaseAction[
+        AvailableSlotsActionConfig, AvailableSlotsParameters, AvailableSlotsResponse
+    ]
+):
+    description: str = "Get available slots by dates for booking"
+    parameters_type: Type[AvailableSlotsParameters] = AvailableSlotsParameters
+    response_type: Type[AvailableSlotsResponse] = AvailableSlotsResponse
+
+    async def run(
+            self, action_input: ActionInput[AvailableSlotsParameters]
+    ) -> ActionOutput[AvailableSlotsResponse]:
+
+        params = {
+            'targetAddress': 'Postal code: ' + action_input.params.postal_code,
+            'filters': json.dumps({'category_id': 1}),
+            'appliance_type': action_input.params.appliance_type if action_input.params.appliance_type else 'electric'}
+        response = requests.get(self.action_config.base_url + '/api/bookings/availableDates', params=params,
+                                headers={'Accept': 'application/json'}).json()
+
+        if response['message'] == 'unknown address':
+            return_val = 'The postal code ' + params['targetAddress'] + ' could not be found.'
+        else:
+            return_val = response['message']
+
+        print('available_slots_response', return_val)
+
+        return ActionOutput(
+            action_type=self.action_config.type,
+            response=AvailableSlotsResponse(success=return_val),
+        )
--- apps/telephony_app/main.py
+++ apps/telephony_app/main.py
@@ -18,11 +18,10 @@ from vocode.streaming.telephony.server.base import (
 )
 from dotenv import load_dotenv
 from vocode.streaming.models.synthesizer import ElevenLabsSynthesizerConfig
-
+from action.available_slots import AvailableSlotsActionConfig
 # Local application/library specific imports
 from speller_agent import (
     SpellerAgentFactory,
-    SpellerAgentConfig,
 )
 
 # if running from python, this will load the local .env
@@ -62,8 +61,34 @@ telephony_server = TelephonyServer(
             url="/inbound_call",
             agent_config=ChatGPTAgentConfig(
                 initial_message=BaseMessage(text="Hello, I am a Man with a wrench virtual agent"),
-                prompt_preamble="Act as a professional and courteous customer service agent for an appliance repair company. Your primary tasks include booking and rescheduling appointments for customers, providing status updates on current service jobs, and offering any relevant information or advice. You will handle each inquiry with empathy and precision, ensuring the customer feels supported throughout their interaction. If a customer asks a question or requests assistance that requires specialized knowledge, provide clear instructions on the next steps they can take or guide them to the appropriate resources",
+                prompt_preamble="""Act as a professional and courteous customer service agent for an appliance repair
+                 company. Your primary tasks include booking and rescheduling appointments for customers, 
+                 providing status updates on current service jobs, and offering any relevant information or advice. 
+                 You will handle each inquiry with empathy and precision, ensuring the customer feels supported 
+                 throughout their interaction. If a customer asks a question or requests assistance that requires 
+                 specialized knowledge, provide clear instructions on the next steps they can take or guide them 
+                 to the appropriate resources.
+                 For booking a repair appointment, here are the steps:
+                 
+                 1. First ask what type of appliance needs repair. 
+                 2. If appliance is dryer or stove ask whether its electric or gas, otherwise no need to ask.
+                 3. Ask the postal code of the repair job location. 
+                 4. Call action_available_slots function to get the available slots for booking an appliance repair job. 
+                 5. Tell customer we provide two timeslots 8am to 2pm and 12pm to 6pm. Suggest first 3 dates of
+                 the available slots.For example if 8am to 2pm slots are available in 1st,2nd,3rd, 
+                 then say 8am to 2pm slots are available for 1st,2nd and 3rd of 2024. Same for 12pm to 6pm.
+                 If customer chosen date are not available, suggest another date close to 
+                 the date customer selected. Do not tell the whole available slots. 
+                 6. Ask additional details for booking such as customer first and last name, address, email, alternative 
+                 phone numbers if any, short description of the problem, appliance model and sticker 
+                 information if any. Ask them these questions one by one. After getting all the relevant details, tell 
+                 them that they will get a confirmation email after all the details are verified.
+                 7. After all the information are available, call the book tool to book the customer repair job.
+                 """,
                 generate_responses=True,
+                actions=[AvailableSlotsActionConfig(
+                    base_url=os.environ.get("BOOKING_BASE_URL")
+                )]
             ),
             # uncomment this to use the speller agent instead
             # agent_config=SpellerAgentConfig(
@@ -71,8 +96,8 @@ telephony_server = TelephonyServer(
             #     generate_responses=False,
             # ),
             synthesizer_config=ElevenLabsSynthesizerConfig.from_telephone_output_device(
-                        api_key=os.getenv("ELEVEN_LABS_API_KEY"),
-                        voice_id="3gsg3cxXyFLcGIfNbM6C"
+                api_key=os.getenv("ELEVEN_LABS_API_KEY"),
+                voice_id="XrExE9yKIg1WjnnlVkGX"
             ),
             twilio_config=TwilioConfig(
                 account_sid=os.environ["TWILIO_ACCOUNT_SID"],
